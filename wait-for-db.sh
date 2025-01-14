4409a5f6d7fa653e25847270a27b8d1661fa6678
Pablo del Pozo <pablodelpozovdt@gmail.com>
2025-01-14 18:08:20 +0100

    feat: change db name to deploy en production server

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# echo "Esperando a que la base de datos esté lista..."
-# while ! nc -z db 5432; do
-  # sleep 0.1
-# done
-# echo "Base de datos lista"
-#!/bin/bash
-
 echo "Esperando a que la base de datos esté lista..."
 while ! nc -z postgres.railway.internal 5432; do
   sleep 0.1
